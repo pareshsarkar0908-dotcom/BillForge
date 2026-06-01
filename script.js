39f4139b83485a4ee051797ecc5d479d8e434423
pareshsarkar0908-dotcom <pareshsarkar0908@gmail.com>
2026-06-01 14:01:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ const appConfig = {
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZodWpqdnp2eHVvYXZ2cmZhZ2lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyOTM2MTksImV4cCI6MjA5NTg2OTYxOX0.bkTCR0b5Jr2nCjyKZJBLHuqsWOPvQ2vY2RoD0JS7KKw",
   razorpayKeyId: "rzp_live_SvxaS8NNrTTmVP",
   // Production note: create Razorpay orders on a secure backend or Supabase Edge Function.
-  createOrderEndpoint: "",
+  createOrderEndpoint: "/api/create-razorpay-order",
 };
 
 const plans = {
